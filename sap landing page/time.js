cb1aaf24ef1340cb1b020481e519e8c6fe1cfe91
Vaibhav Raj Singh <vaibhavrajsingh2001@gmail.com>
2019-12-13 15:44:38 +0530

    Add files via upload
    SAP Landing page

---
+++ sap landing page/time.js
@@ -0,0 +1,26 @@
+const year = new Date().getFullYear();
+const start_date = new Date((year + 1), 1, 14).getTime();
+
+let timer = setInterval(function () {
+
+    const today = new Date().getTime();
+    const diff = start_date - today;
+
+    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    // display
+    document.getElementById("timer").innerHTML =
+        "<div class=\"days\"> \
+  <div class=\"numbers\">" + days + "</div>days</div> \
+<div class=\"hours\"> \
+  <div class=\"numbers\">" + hours + "</div>hours</div> \
+<div class=\"minutes\"> \
+  <div class=\"numbers\">" + minutes + "</div>minutes</div> \
+<div class=\"seconds\"> \
+  <div class=\"numbers\">" + seconds + "</div>seconds</div> \
+</div>";
+
+}, 1000);
