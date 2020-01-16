f5f92c53960701f5f735fb0a10c283e51cc1b929
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2020-01-16 19:51:01 +0530

    err fixed in update password

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 const { isEmail } = require('validator');
 const verifyToken = require('../utils/verifyToken');
