8491cafd605a4c849a80cf7792e525c2f4153d65
Vaibhav Raj Singh <vaibhavrajsingh2001@gmail.com>
2020-01-16 23:57:02 +0530

    more events table

--- Frontend/js/dashboard.js
+++ Frontend/js/dashboard.js
@@ -61,8 +61,8 @@ const url = "https://bitotsav.in/api";
     }
 });*/
 
-var allevents;
-var userEvents;
+const allevents;
+const userEvents;
 
 $.ajax({
     url: url + "/events/allEvents",
@@ -97,7 +97,7 @@ $.ajax({
             userEvents = res.user.soloEventsRegistered;
         }
         for(i=0; i<userEvents.length; i++){
-            var isEventLead = userEvents[i].eventLeaderBitotsavId === res.user.bitotsavId ;
+            let isEventLead = userEvents[i].eventLeaderBitotsavId === res.user.bitotsavId ;
             eventlist(userEvents[i].eventId,userEvents[i].eventName,userEvents[i].eventLeaderBitotsavId,isEventLead);
         }
     }
