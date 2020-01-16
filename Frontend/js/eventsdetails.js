2e0fd418acc395d223681d5dd4508878453ac7ae
Rahul Kumar Patro <rahulkpatro@gmail.com>
2020-01-16 23:57:19 +0530

    updated eventsdetails with json

--- Frontend/js/eventsdetails.js
+++ Frontend/js/eventsdetails.js
@@ -20,8 +20,8 @@ var url1= "https://bitotsav.in";
                 crossDomain:true,
                 success: function(res) {
                     for (i = 0; i < res.data.length; i++) {
-                    $(".card-deck").append(eventdetails(res.data[i], i));
-                   
+                    $(".wrap").append(eventdetails(res.data[i], i, s));
+                    
                 }
             },
             error: function(err) {
@@ -30,69 +30,59 @@ var url1= "https://bitotsav.in";
         
         });
         
-        function eventdetails(events, i) {
+        function eventdetails(events, i , s) {
             return (`  
-            
-                            <div class="card text-center col-md-4 col-xs-12">
-                                <img class="card-img-top" src="images/Events/${events.name}.jpg" onerror=this.src="images/Events/${events.name}.png" onerror=this.src="images/Events/${events.name}.jpeg" alt="Card image">
-                                <br>
-                                <h1 style="color:white; font-size:2em; font-family: 'Courier New', Courier, monospace;">${events.name}</h1>
-                                <div class="card-body">
-                                    <a href="#" class="btn btn-outline-success stretched-link" data-toggle="modal" data-target="#events${i}Modal">More Details</a>
-                                </div>
+           
+            <div class="box two">
         
-        
-        
-                                <!-- The Modal -->
-                                <div class="modal" id="events${i}Modal">
-                                    <div class="modal-dialog modal-lg">
-                                        <br>
-                                        <div class="modal-content">
-        
-                                            <!-- Modal Header -->
-                                            <div class="modal-header ">
-                                                <h4 class="modal-title text-center" style="font-family: 'Courier New', Courier, monospace; font-size:2.5em;">${events.name}</h4>
-                                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                            </div>
-        
-                                            <!-- Modal body -->
-                                            <div class="modal-body">
-                                                <div class="row">
-                                                    
-                                                   <div class="col-md-12 col-12">
-                                                       
-                                                        <strong class="text-center" style="font-size:1.8em; font-family: 'Courier New', Courier, monospace;">CATEGORY:</strong> <h4>${events.category}</h4>
-                                                        <br>
-                                                        <strong class="text-center" style="font-size:1.8em; font-family: 'Courier New', Courier, monospace;">VENUE:</strong> <h4>${events.venue}</h4>
-                                                        <br>
-                                                        <strong class="text-center" style="font-size:1.8em; font-family: 'Courier New', Courier, monospace;">DURATION:</strong> <h4>${events.duration}</h4>
-                                                        <br>
-                                                        <strong class="text-center" style="font-size:1.8em; font-family: 'Courier New', Courier, monospace;">FACULTY ADVISORS:</strong> <h4>${events["faculty advisors"]}</h4>
-                                                        <br>
-                                                        <strong class="text-center" style="font-size:1.8em; font-family: 'Courier New', Courier, monospace;">CLUB:</strong> <h4>${events.club}</h4> 
-                                                        <br>
-                                                        <strong class="text-center" style="font-size:1.8em; font-family: 'Courier New', Courier, monospace;">DESCRIPTION:</strong> <h4 style="letter-spacing:0.5mm;">${events.description}</h4>
-                                                        <br>
-                                                        <strong class="text-center" style="font-size:1.8em;font-family: 'Courier New', Courier, monospace;">RULES AND REGULATIONS:</strong> <h4 style="letter-spacing:0.5mm;">${events["rules and regulations"]}</h4>
-                                                        <br>
-                                                        <br>
-                                                        <strong class="text-center" style="font-size:1.8em; font-family: 'Courier New', Courier, monospace;">CONTACT INFORMATION:</strong> <h4>${events["contact information"]}</h4>
-                                                        <br>
-                                                        <strong class="text-center" style="font-size:1.8em; font-family: 'Courier New', Courier, monospace;">RESOURSES REQUIRED:</strong> <h4 style="letter-spacing:0.5mm;">${events["resources required"]}</h4>
-                                                        <br>
-                                                    </div>
-                                                </div>
-                                            </div>
-        
-                                           
-        
-                                        </div>
-                                    </div>
-                                </div>
-                             </div>  
-                   
+       
+            <h1>${events.name}</h1>
+            <br>
+             <h2> (Events conducted by ${events.eventCategory})</h2>
+           
+             <br><br>
+
+             <a class="button" href="#events${i}Modal" style="color:black; font-size:0.8em; font-weight:800;">More Details</a>
+             <div class="popup" id="events${i}Modal">
+               <div class="popup-inner">
+                 <div class="popup__photo">
+                   <img src="images/Events/${events.name}.jpg" onerror=this.src="images/Events/${events.name}.png" onerror=this.src="images/Events/${events.name}.jpeg" alt="Card image" class="img-responsive">
+                 </div>
+                 <div class="popup__text">
+                   <strong style="font-size:2.3em; color:black;">${events.name}</strong>
+                   <br><br> 
+                   <p> <strong><span class="text">EVENT CATEGORY:</span><span class="text-inner">${events.category}</span></strong>
+                     <br><br>
+                   <strong><span class="text">VENUE:</span><span class="text-inner">${events.venue}</span></strong>
+                   <br><br>
+                   <strong><span class="text">DURATION:</span><span class="text-inner">${events.duration}</span></strong>
+                   <br><br>
+                   <strong><span class="text">DESCRIPTION:</span><span class="text-inner">${events.description}</span></strong>
+                   <br><br>
+                   <strong><span class="text">RULES AND REGULATIONS:</span><span class="text-inner">${events["rules and regulations"]}</span></strong>
+                   <br><br>
+                   <strong><span class="text">CONTACT INFORMATION:</span><span class="text-inner">${events["contact information"]}</span></strong>
+                   <br><br>
+                   </p>
+                 </div>
+                 <a class="popup__close" href="#">X</a>
+               </div>
+             </div>
+
+
+          </div>
                               
         `);
         }
 
 
+        $(document).ready(function(){
+            $(".button").click(function(){
+              $("#events${i}Modal").css({"visibilty": " visible", "opacity":"1"});
+            });
+          });
+
+
+          $('.two').css({'background': 'url(../images/Events/Dhwani.jpg)', 'background-repeat': 'no repeat'});
+          
+        
