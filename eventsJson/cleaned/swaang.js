f7f1051b79bb9aa8e0802f7082b470c7e902acc1
priyanshu2510 <priyanshu2510kr@gmail.com>
2020-01-19 00:47:03 +0530

    json cleaned

---
+++ eventsJson/cleaned/swaang.js
@@ -0,0 +1,139 @@
+const swaang = [
+	{
+		"id": 7,
+		"name": `PUKAAR`,
+		"eventCategory": `SWAANG`,
+		"imageName": ``,
+		"category": `Flagship`,
+		"points": `None`,
+		"venue": `Shorbagh-1`,
+		"duration": `2 days, 5 hours`,
+		"faculty advisor": `Dr. Sandeep Kumar Singh`,
+		"club": `Ehsaas Dramatcs Society`,
+		"description": `This is a street play competton. What beter way than Nukkad to express our views. The voice of our conscience echoes through the words of Nukkad. The evils of the society are brought into light through the plot of Nukkad. Let us rewind the days when some scintllatng performances ignited the fre for change in us.
+        `,
+		"rules and regulations": `This competton will be conducted in two rounds, eliminatons and fnals. Both rounds will be held on two dioerent days of the fest.
+        General Rules:
+        1. The team shall consist of maximum of 20 people excluding 2 instrumentalists (optonal).
+        2. The tme limit should be strictly adhered to. In case of violaton, partcipants will be penalized by judges. The given tme duraton is from empty stage to empty stage.
+        3. Teams are expected to perform at an open-air venue, on a circular stage with audience on all sides.
+        4. Multple teams from the same college are permited.
+        5. No hazardous substances are allowed. (fluids and fre)
+        6. Partcipatng teams are required to send in a sot copy of their scripts along with details (names of members, college name, props being used, instruments) at events@bitotsav.in before 5th February 2020 (11:59 pm).
+        7. Obscenity of any kind is prohibited.
+        8. For live music teams will have to bring their own instruments but no electronic device or recorded music.
+        9. The languages permited are Hindi and English.
+        10.Judging Criterion:
+        i) 30 % - Content
+        ii) 25 % - Actng/ Expression
+        iii) 20 % - Audience Response
+        iv) 15 % - Directon
+        v) 10 % - Miscellaneous Eoects
+        11.The decision of the judging panel shall remain fnal and binding.
+        Eliminatons:
+        The tme limit shall be 12+2 minutes.
+        Finals:
+        The tme limit shall be 20+2 minutes.
+        `,
+		"contact information": `1. Shivam: 9523126405
+        2. Shambhavi: 8840358232
+`,
+		"resources required": `1. Table and chairs
+        2. Judges score sheet
+        3. Judges refreshment
+        4. Water
+`,
+"imageName": `none.jpg`, "individual": `1`,
+"minParticipants": "",
+"maxParticipants": "",
+"cashPrize": ""
+	}, {
+		"id": 8,
+		"name": `ABHINAY`,
+		"eventCategory": `SWAANG`,
+		"imageName": ``,
+		"category": `Flagship`,
+		"Points": `None`,
+		"venue": `G.P. Birla Auditorium`,
+		"duration": `2 days, 6 hours`,
+		"faculty advisor": `Dr. Sandeep Kumar Singh`,
+		"club": `Ehsaas Dramatics Society`,
+		"description": `This is a stage play competition. If you think you can mesmerize the audience with acting, script, dialogues and sets - then Abhinay is your call! It cannot get better than the annual stage play competition at Bitotsav. Once the stage is set and all is ready - it is show time.
+        `,
+		"rules and regulations": `This competition will be conducted in two rounds, eliminations and finals. Both rounds will be held on two different days of the fest.
+        General rules:
+        1. Each team should have a minimum of 5 and a maximum of 10 actors. In addition, there can be 1 person in charge of music and a maximum of 2 people for lights.
+        2. No hazardous substances are allowed.
+        3. The language used can be English, Hindi or both.
+        4. Participating teams are required to send in a soft copy of their scripts along with details (name of members, college name, props being used) at events@bitotsav.in before 5th February 2020 (11:59 pm).
+        5. Multiple teams from the same college are permitted.
+        6. No hazardous substances are allowed.
+        7. The decision of the judges shall be final and binding.
+        Eliminations:
+        The time duration is 20 minutes which includes performance time plus 3 minutes of stage setup time. The given time duration is from empty stage to empty stage.
+        Finals:
+        The time duration is 50 minutes which includes performance time and stage setting time. The given time duration is from empty stage to empty stage.
+        `,
+		"contact information": `1. Shivam: 9523126405
+        2. Shambhavi: 8840358232
+`,
+		"resources required": `1. Table and chairs
+        2. Judges score sheet
+        3. Judges refreshment
+        4. Water
+`,
+"imageName": `none.jpg`, "individual": `1`,
+"minParticipants": "",
+"maxParticipants": "",
+"cashPrize": ""
+	},
+	{
+		"id" : 9,
+		"name": `NATSAMRAAT`,
+		"eventCategory": `SWAANG`,
+		"imageName": ``,
+		"category": `Formal`,
+		"points": `NOne`,
+		"venue": `G.P. Birla Auditorium`,
+		"duration": `3 hours`,
+		"faculty advisor": `Dr. Sandeep Kumar Singh`,
+		"club": `Ehsaas Dramatics Society`,
+		"description": `Do you like to fly solo? Do you like that feeling when the spotlight hits you and the whole scene revolves around you? So, here is your chance to prove your talent at mono-acting. Prepare yourself to single-handedly dazzle the audience.
+        `,
+		"rules and regulations": `This event will be held in two rounds consisting of an online round followed by further rounds at BIT Mesra, Ranchi main campus.
+        General Rules:
+        1. This is a solo participation event.
+        2. The participant can prepare a monologue or solo act of their choice.
+        3. The act must be Hindi or English or both. Incorporation of short passages from other languages is allowed.
+        4. Multiple participants from the same college are permitted.
+        5. Obscenity of any kind in the content or actions in the act will lead to immediate disqualification of the participant.
+        6. Decision of the judges shall remain final and binding.
+        Online Round:
+        1. The participant is required to send in a video clip of not more than 2 minutes showcasing their talent along with details (name, college name) at events@bitotsav.in. The video size should not be more than 50 MB.
+        2. The last date for sending the entries will be 2nd February 2020 (11:59 pm).
+        3. The results will be announced on or before 5th February 2020. The participants selected from the online round are required to report at BIT Mesra, Ranchi main campus.
+        Further Rounds:
+        1. Further rounds will be held in an auditorium.
+        2. A maximum of 2 people can support the participant for sound and light operation. They must strictly remain off-stage.
+        3. The participant can use a maximum of one prop in the performance.
+        4. The performance must consist of a single act with no blackouts.
+        5. The time limit for each performance is 6 minutes (max). Points will be deducted for exceeding the time limit.
+        `,
+		"contact information": `1. Shivam: 9523126405
+        2. Shambhavi: 8840358232
+`,
+		"resources required": `1. Table and chairs
+        2. Judges score sheet
+        3. Judges refreshment
+        4. Water
+`,
+		"imageName": `none.jpg`, "individual": `1`,
+		"minParticipants": 1,
+		"maxParticipants": 1,
+		"cashPrize": ""
+		}
+		
+	
+]
+
+module.exports = swaang ;
