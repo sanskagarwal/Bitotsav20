0b32f55e11e12ece8e3a3ffce988b0e0cf76916f
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2020-01-20 18:20:46 +0530

    seed entry and font in profile

---
+++ seed.js
@@ -0,0 +1,105 @@
+// Database seed file (Proceed with caution!!)
+
+// Models
+const eventModel = require('./models/events');
+const coreTeamModel = require('./models/coreTeam');
+const bitIdCounter = require('./models/bitIdCounter');
+const sapIdCounter = require('./models/sapIdCounter');
+const teamIdCounter = require('./models/teamIdCounter');
+
+// Json
+const dhwani = require('./eventsJson/cleaned/dhwani');
+const dansation = require('./eventsJson/cleaned/dansation');
+const adaa = require('./eventsJson/cleaned/adaa');
+const digitales = require('./eventsJson/cleaned/digitales');
+const euphoria = require('./eventsJson/cleaned/euphoria');
+const herald = require('./eventsJson/cleaned/herald');
+const meraki = require('./eventsJson/cleaned/meraki');
+const rhetoric = require('./eventsJson/cleaned/rhetoric');
+const swaang = require('./eventsJson/cleaned/swaang');
+
+const connectDB = require('./db/mongoose_connection');
+connectDB();
+
+const req = Number(process.argv[2]);
+console.log(`Requested route: ${req}`);
+
+if (req === 1) {
+    teamIdCounter.find({}, (err, docs) => {
+        if (err) {
+            return console.log(err);
+        }
+        if (!docs) {
+            teamIdCounter.insertOne({ count: 200001 }, (err, save) => {
+                if (err) {
+                    return console.log(err);
+                }
+                return console.log("Inserted");
+            });
+        }
+    });
+} else if (req === 2) {
+    sapIdCounter.find({}, (err, docs) => {
+        if (err) {
+            return console.log(err);
+        }
+        if (!docs) {
+            sapIdCounter.insertOne({ count: 2000001 }, (err, save) => {
+                if (err) {
+                    return console.log(err);
+                }
+                return console.log("Inserted");
+            });
+        }
+    });
+} else if (req === 3) {
+    bitIdCounter.find({}, (err, docs) => {
+        if (err) {
+            return console.log(err);
+        }
+        if (!docs) {
+            bitIdCounter.insertOne({ count: 200001 }, (err, save) => {
+                if (err) {
+                    return console.log(err);
+                }
+                return console.log("Inserted");
+            });
+        }
+    });
+} else if (req === 4) {
+    console.log("It will delete events");
+    const events = [...dhwani, ...dansation, ...adaa, ...digitales, ...euphoria, ...herald, ...meraki, ...rhetoric, ...swaang];
+    eventModel.deleteMany({}, (err, res) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Events Deleted");
+        eventModel.insertMany(events)
+            .then(() => {
+                return console.log("Inserted Events");
+            })
+            .catch((error) => {
+                return console.log(error);
+            });
+    });
+} else if (req === 5) {
+    console.log("It will delete Team");
+
+    let teamJson = require('./teamJson/team.json');
+    teamJson = [...teamJson];
+    coreTeamModel.deleteMany({}, (err, res) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Team Deleted");
+        coreTeamModel.insertMany(teamJson)
+            .then(() => {
+                return console.log("Inserted Team");
+            })
+            .catch((error) => {
+                return console.log(error);
+            })
+    });
+} else {
+    console.log("Check Number");
+}
