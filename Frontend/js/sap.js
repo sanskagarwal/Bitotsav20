edb6442752fdfecaa0ec4875c951043aac67e703
aryanAbhishek18 <aryan.abhishek1829@gmail.com>
2020-01-20 21:30:52 +0530

    Header in events page

--- Frontend/js/sap.js
+++ Frontend/js/sap.js
@@ -1,13 +1,3 @@
-
-// Preloader (if the #preloader div exists)
-$(window).on('load', function () {
-  if ($('#preloader').length) {
-    $('#preloader').delay(100).fadeOut('slow', function () {
-      $(this).remove();
-    });
-  }
-});
-
 // Back to top button
 $(window).scroll(function () {
   if ($(this).scrollTop() > 100) {
