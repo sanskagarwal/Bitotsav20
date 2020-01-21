4e15a61e9426acd7a69029ebbd08a1177a4be772
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2020-01-21 12:10:33 +0530

    sender id changed in sms

--- utils/sendPM.js
+++ utils/sendPM.js
@@ -2,7 +2,7 @@ const config = require('./../config');
 const axios = require('axios');
 
 const sendPM = (message, phoneNo) => {
-    let otpUrl = `http://sms.digimiles.in/bulksms/bulksms?username=${config.digimiles.username}&password=${config.digimiles.password}&type=0&dlr=1&destination=${phoneNo}&source=PANTHN&message=${message}`;
+    let otpUrl = `http://sms.digimiles.in/bulksms/bulksms?username=${config.digimiles.username}&password=${config.digimiles.password}&type=0&dlr=1&destination=${phoneNo}&source=BITOSV&message=${message}`;
 
     axios.get(otpUrl)
         .then(function (response) {
