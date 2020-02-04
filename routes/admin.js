dfa7093713e0c1aaa5c1d07dccdeb724d5470634
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2020-02-04 17:03:15 +0530

    access to publicity

--- routes/admin.js
+++ routes/admin.js
@@ -271,8 +271,9 @@ router.post('/updateEventById', (req, res, next) => {
 });
 
 router.post("/getTeamsByEventId", async (req, res) => {
-    const valid = adminAuth('events', req.body.password);
-    if (!valid) {
+    const validForEventsTeam = adminAuth('events', req.body.password);
+    const validForPublicityTeam = adminAuth('publicity', req.body.password);
+    if (!validForEventsTeam && !validForPublicityTeam) {
         return res.json({
             status: 401,
             message: "Not Authorised"
